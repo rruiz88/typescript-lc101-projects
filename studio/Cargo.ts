03291ff876e2fecd3fa3aae521d9f769718e76f6
rruiz88 <roman.ruiz816@gmail.com>
2022-08-15 21:00:00 -0500

    Studio completed.

---
+++ studio/Cargo.ts
@@ -0,0 +1,16 @@
+import { Payload } from './Payload';
+
+
+
+export class Cargo implements Payload{
+    massKg: number;
+    material: string;
+
+    constructor(massKg: number, material: string){
+this.massKg = massKg;
+this.material = material;
+
+    }
+
+
+}
